f15f7e21a4a865158f3b63d51bd7a814ca656619
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-09-15 12:53:58 -0400

    Create Projects.js

---
+++ js/Projects.js
@@ -0,0 +1,88 @@
+class Projects {
+    constructor() {
+        this.projects = [
+            {
+                "projectName": "Full Stack React App with Rest API",
+                "description": "In this app, a user interface is created with React for a school database Rest API. This full stack application provides a way for users to interact with the information held within the database. Users can view a list of the courses, and view each course's detail. Those who have an account can sigh-in to their accounts to create, update, and delete their courses. If users don't have an account they can easily sign-up.",
+                "technologies": ["React", " JSX", " React Context API", " Rest API", " React App", " JavaScript" ],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/Full-Stack-App-with-React-and-a-REST-API"
+            },
+            {
+                "projectName": "Rest API",
+                "description": "This Rest API was created using Express. It provides a way for users to administer a school database containing information about courses. Each user can interact with the database by retrieving a list of courses, adding courses, as well as updating and deleting courses in the database. In order to make changes to the database the user must have an account or create a new account. From there, the user will only have access to courses that are owned by them.",
+                "technologies": ["Node.js", " Express.js", " MySQL", " Basic-Auth", " Bcryptjs", " JavaScript" ],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/Rest-API"
+            },
+            {
+                "projectName": "SQL Library Manager",
+                "description": "This dynamic web application manages a SQLite database for a fictional library. The application displays the itesm in the database and is able to create new book entries, edit current book entries, and delete any entry.",
+                "technologies": ["Node.js", " Express.js", " Pug", " MySQL", " JavaScript" ],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/SQL-Library-Manager"
+            },
+            {
+                "projectName": "React App",
+                "description": "Image gallery app that is built with a modern single page applications to keep it fast, modular, and in sync with current web development trends. Axios is used to fetch data from the Flickr API, so that the images can be used to display.",
+                "technologies": ["React", " JSX", " Flickr API", " JavaScript" ],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/React-Gallery-App"
+            },
+            {
+                "projectName": "Static Node.js and Express Site",
+                "description": "This project uses server-side software to create a portfolio site that showcases the first 5 projects I created as a Full Stack JavaScript Developer student at Treehouse.",
+                "technologies": ["Express.js", " Node.js", " JavaScript", " HTML", " CSS" ],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/Static-Node.js-and-Express-Site"
+            },
+            {
+                "projectName": "Public API Requests",
+                "description": "A fictional company called Awesome Startup needed an app to share contact information. This app grabs 12 random employees from a Random User Generator API and requests a JSON object to parse. The 12 employees are displayed along with their information. Each employee can be found when searched and show more information when clicked on.",
+                "technologies": ["Random User API", " JSON", " JavaScript", " HTML", " CSS" ],
+                "liveLink": "https://jimmikedave.github.io/treehouse-project-5/",
+                "githubLink": "https://github.com/jimmikedave/treehouse-project-5"
+            },
+            {
+                "projectName": "OOP Typing Game",
+                "description": "Browser-based typing game. This game is created with JavaScript and OOP (Object-Oriented Programming). The player type the random phase selected without making any errors and must complete the phrase presented before a timer runs out. Survive as many rounds as you can!",
+                "technologies": ["JavaScript", " HTML", " CSS"],
+                "liveLink": "",
+                "githubLink": "https://github.com/jimmikedave/Type-For-Your-Life"
+            },
+            {
+                "projectName": "OOP Game Show App",
+                "description": "Browser-based word guessing game. This game is created with JavaScript and OOP (Object-Oriented Programming). A random phrase is selected and the player tries to guess what the word is by clicking or typing the letter they would like to guess. If you run out of hearts you lose!",
+                "technologies": ["JavaScript", " HTML", " CSS"],
+                "liveLink": "https://jimmikedave.github.io/Treehouse-Project-4/",
+                "githubLink": "https://github.com/jimmikedave/Treehouse-Project-4"
+            },
+            {
+                "projectName": "Interactive Form",
+                "description": "Interactive form for a fictional conference called FullStack Conf. Conditional behavior is added with JavaScript to create a user-friendly program. If user input is invalid, error indicators appear on the page for the user to enter valid data.",
+                "technologies": ["JavaScript", " HTML", " CSS"],
+                "liveLink": "https://jimmikedave.github.io/Treehouse-Project-3/",
+                "githubLink": "https://github.com/jimmikedave/Treehouse-Project-3"
+            },
+            {
+                "id": "3",
+                "projectName": "List Pagination and Filtering",
+                "description": "A list of 54 student names is shown through list pagination and filtering. 10 names are displayed on each page through JavaScript. On the bottom of the page, users can cycle through the list by clicking the designated page number.",
+                "technologies": ["JavaScript", " HTML", " CSS"],
+                "liveLink": "https://jimmikedave.github.io/Treehouse-Project-2/",
+                "githubLink": "https://github.com/jimmikedave/Treehouse-Project-2"
+            },
+            {
+                "id": "4",
+                "projectName": "Random Quote Generator",
+                "description": "A quote within an array is chosen at random to be displayed. Another quote within the array can be shown at random if the Show another quote button is clicked. Every time the quote changes the background color changes at random as well. A new quote will appear every 10 seconds.",
+                "technologies": ["JavaScript", " HTML", " CSS"],
+                "liveLink": "https://jimmikedave.github.io/Treehouse-Project-1/",
+                "githubLink": "https://github.com/jimmikedave/Treehouse-Project-1"
+            }
+        ];
+    }
+};
+
+
+
