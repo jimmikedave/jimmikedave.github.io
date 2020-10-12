43c57628e90896bd6462dbcfa83782b4162b5122
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-10-12 17:32:54 -0400

    Update Projects.js
    Remove projects and update descriptions

--- js/Projects.js
+++ js/Projects.js
@@ -2,30 +2,30 @@ class Projects {
     constructor() {
         this.projects = [
             {
-                "projectName": "Full Stack React App with Rest API",
-                "description": "In this app, a user interface is created with React for a school database Rest API. This full stack application provides a way for users to interact with the information held within the database. Users can view a list of the courses, and view each course's detail. Those who have an account can sign-in to their accounts to create, update, and delete their courses. If users don't have an account they can easily sign-up.",
-                "technologies": ["React", " JSX", " React Context API", " Rest API", " React App", " JavaScript" ],
+                "projectName": "Full Stack React App with REST API",
+                "description": "A user interface is created for a school course application with React, along with a REST API for the SQL database created with Node.js and Express. The database contains the course table and the user table. Users can view a list of the courses, and each course's detail. Those who have an account can create, update, and delete courses of their own. If a user does not have an account they can easily sign-up.",
+                "technologies": ["React", " Node.js", " Express", " JavaScript" ],
                 "liveLink": [],
                 "githubLink": "https://github.com/jimmikedave/Full-Stack-App-with-React-and-a-REST-API"
             },
             {
                 "projectName": "OOP Typing Game",
-                "description": "Browser-based typing game. This game is created with JavaScript and OOP (Object-Oriented Programming). The player types the random phase selected without making any errors and must complete the phrase presented before a timer runs out. Survive as many rounds as you can!",
+                "description": "Browser-based typing game created with JavaScript and Object-Oriented Programming. The objective of the game is to type the phrase that is presented without making any errors and before the timer runs out. After each phrase has been completed, a new random phrase will appear until the player loses. An on-screen keyboard shows when a key is pressed and the amount of rounds the player survives will be given at the end of the game.",
                 "technologies": ["JavaScript", " HTML", " CSS"],
                 "liveLink": ["https://jimmikedave.github.io/Type-For-Your-Life/"],
                 "githubLink": "https://github.com/jimmikedave/Type-For-Your-Life"
             },
             {
                 "projectName": "React Gallery App",
-                "description": "Image gallery app that is built with a modern single page applications to keep it fast, modular, and in sync with current web development trends. Axios is used to fetch data from the Flickr API, so that the images can be used to display.",
-                "technologies": ["React", " JSX", " Flickr API", " JavaScript" ],
+                "description": "Image gallery application that is built with a modern single page application to keep it fast, modular, and in sync with current web development trends. Axios is used to fetch data from the Flickr API. You can display images based on the main tabs or search for a category of images.",
+                "technologies": ["React", " Flickr API", " JavaScript", " JSX"  ],
                 "liveLink": [],
                 "githubLink": "https://github.com/jimmikedave/React-Gallery-App"
             },
             {
                 "projectName": "Public API Requests",
-                "description": "A fictional company called Awesome Startup needed an app to share contact information. This app grabs 12 random employees from a Random User Generator API and requests a JSON object to parse. The 12 employees are displayed along with their information. Each employee can be found when searched and show more information when clicked on.",
-                "technologies": ["Random User API", " JSON", " JavaScript", " HTML", " CSS" ],
+                "description": "This application parses 12 random users, JSON objects, from the Random User Generator API and generates an employee directory. Each employee card displays basic information if clicked and can be found when searched.",
+                "technologies": [ "Random User API", " JavaScript", " JSON", " HTML", " CSS" ],
                 "liveLink": ["https://jimmikedave.github.io/treehouse-project-5/"],
                 "githubLink": "https://github.com/jimmikedave/treehouse-project-5"
             },
