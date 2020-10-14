376b2f4b7c3f19912ef163c28e5dc27c212dc273
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-10-14 09:26:55 -0400

    Update app.js
    Add jQuery for smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+    var scrollLink = $('.scroll');
+
+    // Smooth scrolling
+    scrollLink.click(function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            // How far away from the top is the hash
+            scrollTop: $(this.hash).offset().top
+        }, 1000)
+    })
+
+})
+
+
 /* Display projects */
 
 // Call the Projects() class
